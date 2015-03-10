c7095eef69a3b29423d9240602eaf0208d8c321f
hamzacooly <hamzakhatri@utexas.edu>
2015-03-10 15:31:17 -0500

    Testing

--- src/prob10.java
+++ src/prob10.java
@@ -5,4 +5,5 @@ public class prob10 {
     public static void main(String[] args){
 
     }
+
 }
--- src/prob12.java
+++ src/prob12.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 /**
  * Created by Hamza on 3/7/2015.
  */
 public class prob12 {
     public static void main(String[] args){
-
+        
     }
 }
